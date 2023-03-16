fc866b79de99362e13ccb6af425ee714ad3161bc
GNX56S <ilhanjuliansyah3@gmail.com>
2023-03-16 20:55:48 +0700

    menambahkan CRUD di controllers Pelayanan

--- route/pelayanan.js
+++ route/pelayanan.js
@@ -3,6 +3,8 @@ import Pelayanan from "../controllers/Pelayanan.js";
 
 const pelayanan = express.Router()
 pelayanan.get('/',Pelayanan.get)
+pelayanan.post('/',Pelayanan.post)
+pelayanan.delete('/:id',Pelayanan.del)
 
 
 export default pelayanan
