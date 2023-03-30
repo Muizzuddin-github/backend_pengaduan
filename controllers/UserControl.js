6d6c22025d15821c210e5dfc8f6dc7dcc24b0a1c
Muizzuddin-github <muizzuddin334@gmail.com>
2023-03-30 21:30:38 +0700

    perbaikan nama file User to UserControl

---
+++ controllers/UserControl.js
@@ -0,0 +1,114 @@
+import { PrismaClient } from "@prisma/client"
+import UserVal from "../validation/UserVal.js"
+import bcryptjs from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+class UserControl{
+    static async get(req,res){
+        try{
+
+            const user = await prisma.users.findMany({
+                select : {
+                    id : true,
+                    username : true,
+                    email: true,
+                    tanggal_daftar: true,
+                    roles : {
+                        select : {
+                            role : true
+                        }
+                    }
+                }
+            })
+            return res.status(200).json({
+                status : "OK",
+                message : "semua data user",
+                errors : [],
+                data : user
+            })
+
+        }catch(err){
+            return res.status(500).json({
+                status : "Internal Server Error",
+                message : "terjadi kesalahan diserver",
+                errors : [err.message],
+                data : []
+            })
+        }
+    }
+
+    static async post(req,res){
+        try{
+
+            const checkContentType = req.is('application/json')
+            if(!checkContentType){
+                return res.status(400).json({
+                    status : "Bad Request",
+                    message : "terjadi kesalahan diclient",
+                    errors : ["content type harus application/json"],
+                    data : []
+                })
+            }
+
+            const val = new UserVal(req.body)
+            val.checkType()
+
+            if(val.getErrors().length){
+                return res.status(400).json({
+                    status : "Bad Request",
+                    message : "terjadi kesalahan diclient",
+                    errors : val.getErrors(),
+                    data : []
+                })
+            }
+            
+            val.checkLen()
+            val.checkIsEmail()
+            await val.checkUniqEmail()
+            
+            if(val.getErrors().length){
+                return res.status(400).json({
+                    status : "Bad Request",
+                    message : "terjadi kesalahan diclient",
+                    errors : val.getErrors(),
+                    data : []
+                })
+            }
+
+            const salt = bcryptjs.genSaltSync(10)
+            const hashPassword = bcryptjs.hashSync(val.password,salt)
+
+            const user = await prisma.users.create({
+                data : {
+                    username : val.username,
+                    email : val.email,
+                    password : hashPassword
+                },
+                select : {
+                    id : true,
+                    username : true,
+                    email : true,
+                    tanggal_daftar : true
+                }
+            })
+            
+            return res.status(201).json({
+                status : "Created",
+                message : "berhasil menambahkan user",
+                errors : [],
+                data : [user]
+            })
+
+        }catch(err){
+            return res.status(500).json({
+                status : "Internal Server Error",
+                message : "terjadi kesalahan diserver",
+                errors : [err.message],
+                data : []
+            })
+        }
+    }
+}
+
+export default UserControl
--- route/user.js
+++ route/user.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import User from '../controllers/User.js'
+import UserControl from '../controllers/UserControl.js'
 import Auth from '../controllers/Auth.js'
 import onlyLogin from '../middlewares/onlyLogin.js'
 
 const user = express.Router()
 
 
-user.get("/",User.get)
-user.post("/",User.post)
+user.get("/",UserControl.get)
+user.post("/",UserControl.post)
 user.post("/login",Auth.login)
 user.get("/logout",Auth.logout)
 user.get("/refresh-access-token",onlyLogin,Auth.refreshAccessToken)
